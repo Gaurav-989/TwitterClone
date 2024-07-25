560241d963a3a2870d8736717dd87706492e1afc
Gaurav-989 <112372322+Gaurav-989@users.noreply.github.com>
2024-07-25 10:32:44 +0530

    Delete middleware.js

--- middleware.js
+++
@@ -1,9 +0,0 @@
-// Middle Ware for Login
-exports.isLoggedIn = function(req, res, next) {
-    if(req.session?.user) {
-        return next();
-    }
-    else {
-        return res.redirect('/login');
-    }
-}
